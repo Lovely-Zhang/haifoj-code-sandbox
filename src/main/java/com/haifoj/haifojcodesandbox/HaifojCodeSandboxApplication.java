721451910c1355fba9d285106101e23befd3a4e9
zhf <2764069380@qq.com>
2023-08-29 00:30:28 +0800

    沙箱-代码仓库初始化

---
+++ src/main/java/com/haifoj/haifojcodesandbox/HaifojCodeSandboxApplication.java
@@ -0,0 +1,13 @@
+package com.haifoj.haifojcodesandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaifojCodeSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HaifojCodeSandboxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haifoj/haifojcodesandbox/HaifojCodeSandboxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haifoj.haifojcodesandbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HaifojCodeSandboxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
